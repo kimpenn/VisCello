44020b29fdee1b7cd8b0eda532da7475e7b1bfe9
Laura Pearlman <laura@ragdoll.isi.edu>
2019-02-12 05:38:28 -0800

    RBK column changes

--- inst/app/src/python_src/rbk_study.py
+++ inst/app/src/python_src/rbk_study.py
@@ -10,13 +10,13 @@ def get_relevant_study_urls(pb, host, instance_rid):
     catalog = server.connect_ermrest(2)
     pb = catalog.getPathBuilder()
     application_file_table = pb.RNASeq.Application_File.alias('F')
-    role_table = pb.RNASeq.Application_File_Role.alias('R')
+    role_table = pb.RNASeq.Application_File_Tag.alias('R')
     path = application_file_table.path.\
-           filter(application_file_table.Application_Instance.eq(instance_rid)).\
+           filter(application_file_table.Visualization.eq(instance_rid)).\
            link(role_table)
     retval = dict()
-    for row in path.entities(path.R.Role_Name, path.F.File_URL):
-        retval[row.get("Role_Name")] = "https://{h}{p}".format(h=host, p=row.get("File_URL"))
+    for row in path.entities(path.R.Tag_Name, path.F.File_URL):
+        retval[row.get("Tag_Name")] = "https://{h}{p}".format(h=host, p=row.get("File_URL"))
     return(retval)
 
             
