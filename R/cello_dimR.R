062a20bc6e35fde7b5c4d96d56125906b66c6aec
Qin Zhu <qinzhu@outlook.com>
2020-06-02 00:35:51 -0400

    Fix pca use_dim

--- R/cello_dimR.R
+++ R/cello_dimR.R
@@ -18,7 +18,7 @@ compute_tsne_cello <- function(eset, cello, use_dim=30, n_component = 2, perplex
     pca_proj <- cello@proj[["PCA"]]
     if(ncol(pca_proj) < use_dim) stop("Compute PCA with dimension greater than that specified in use_dim.")
     
-    tsne_res <- Rtsne::Rtsne(as.matrix(pca_proj), dims = n_component, pca = F, perplexity=perplexity, ...)
+    tsne_res <- Rtsne::Rtsne(as.matrix(pca_proj[,1:use_dim]), dims = n_component, pca = F, perplexity=perplexity, ...)
     tsne_proj <- as.data.frame(tsne_res$Y[, 1:n_component])
     colnames(tsne_proj) <- paste0("TSNE_", 1:n_component)
     rownames(tsne_proj) <- colnames(eset)
@@ -40,7 +40,7 @@ compute_umap_cello <- function(eset, cello, use_dim = 30,
     pca_proj <- cello@proj[["PCA"]]
     if(ncol(pca_proj) < use_dim) stop("Compute PCA with dimension greater than that specified in use_dim.")
     
-    umap_proj <- uwot::umap(as.matrix(pca_proj),
+    umap_proj <- uwot::umap(as.matrix(pca_proj[,1:use_dim]),
                n_components = n_component,
                metric = metric,
                min_dist = min_dist,
